ca8f4fb570a44798d064797af3c18554e326ed58
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-02-08 13:27:42 -0800

    add volume function to php

--- parcel.php
+++ parcel.php
@@ -1,79 +1,43 @@
 <?php
-  class Parcel
-  {
-      private $width;
-      private $height;
-      private $length;
-      private $weight;
-
-    function getVolume() {
-        return $this->width * $this->height * $this->length;
-    }
-
-    // function __construct($width, $height, $length, $weight)
-    // {
-    //     $this->width = $width;
-    //     $this->height = $height;
-    //     $this->length = $length;
-    //     $this->weight = $weight;
-    // }
-
-    function setWidth($newWidth)
+    class Parcel
     {
-        $this->width = $newWidth;
-    }
-    function getWidth()
-    {
-        return $this->width;
+        public $width;
+        public $length;
+        public $height;
+        public $weight;
+
+        // function __construct($width, $length, $height, $weight)
+        // {
+        //     $this->width = $width;
+        //     $this->length = $length;
+        //     $this->height = $height;
+        //     $this->weight = $weight;
+        // }
+
+        function getVolume()
+        {
+            return $this->width * $this->length * $this->height;
+        }
     }
 
-    function setHeight($newHeight)
-    {
-        $this->height = $newHeight;
-    }
-    function getHeight()
-    {
-        return $this->height;
-    }
-
-    function setLength($newLength)
-    {
-        $this->length = $newLength;
-    }
-    function getLength()
-    {
-        return $this->length;
-    }
-
-    function setWeight($newWeight)
-    {
-        $this->weight = $newWeight;
-    }
-    function getWeight()
-    {
-        return $this->weight;
-    }
-  }
-
-    $my_parcel = new Parcel();
-    $my_parcel->setWidth() = $_GET["width"];
-    $my_parcel->setHeight() = $_GET["height"];
-    $my_parcel->setLength() = $_GET["length"];
-    $my_parcel->setWeight() = $_GET["weight"];
+    $my_parcel = new Parcel($width, $length, $height, $weight);
+    $my_parcel->width = $_GET["width"];
+    $my_parcel->length = $_GET["length"];
+    $my_parcel->height = $_GET["height"];
+    $my_parcel->weight = $_GET["weight"];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <title>Shipping Cost</title>
 </head>
 <body>
-    <h1>Your shipping cost:</h1>
+    <h1>This will be your shipping cost:</h1>
     <?php
-        $newWidth = $parcel->getWidth();
-        $newHeight = $parcel->getHeight();
-        $newLength = $parcel->getLength();
-        $newWeight = $parcel->getWeight();
+        $newVolume = $my_parcel->getVolume();
+        echo "<h3> $newVolume </h3>";
     ?>
 </body>
 </html>
